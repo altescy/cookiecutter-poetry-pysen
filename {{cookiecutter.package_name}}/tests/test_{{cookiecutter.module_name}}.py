5f3ca563604a439cd3fe7f61e423c6ca17cc26a4
altescy <altescy@fastmail.com>
2021-09-10 19:27:07 +0900

    use snake-cased module name

---
+++ {{cookiecutter.package_name}}/tests/test_{{cookiecutter.module_name}}.py
@@ -0,0 +1,5 @@
+import {{cookiecutter.module_name}}
+
+
+def test_version() -> None:
+    assert {{cookiecutter.module_name}}.__version__ == "0.1.0"
--- {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
+++
@@ -1,5 +0,0 @@
-import {{cookiecutter.package_name}}
-
-
-def test_version() -> None:
-    assert {{cookiecutter.package_name}}.__version__ == "0.1.0"
